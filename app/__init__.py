64d859a68ae25e73a572d3aaa2c4a1e311e2a081
Felipe <felipemachado.oliveira1@gmail.com>
2022-06-29 00:19:45 -0300

    Criação da tabela de usuários

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 
 app = Flask(__name__)
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = sqlalchemy(app)
 
 from app.controllers import default
