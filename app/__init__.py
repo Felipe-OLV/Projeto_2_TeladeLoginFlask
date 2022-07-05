c48f8e89429510c54c750a3254bf0676ae86c976
Felipe <felipemachado.oliveira1@gmail.com>
2022-07-04 23:02:49 -0300

    Adicionando migrações do db

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
-db = sqlalchemy(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 from app.controllers import default
