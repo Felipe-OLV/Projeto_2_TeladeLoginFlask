9c5dbf6a627668add11dda137f7495acda17bad5
Felipe <felipemachado.oliveira1@gmail.com>
2022-09-05 22:06:56 -0300

    Templates e forms finalizados

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -1,10 +1,16 @@
 from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 class RegistrationForm(Form):
-    nome = StringField('Nome:', [validators.Length(min=4, max=25)])
+    name = StringField('Nome:', [validators.Length(min=4, max=25)])
+    username = StringField('Usuario:', [validators.Length(min=4, max=25)])
     email = StringField('Email:', [validators.Length(min=6, max=35)])
-    senha = PasswordField('Digite sua senha:', [
+    password = PasswordField('Digite sua senha:', [
         validators.DataRequired(),
-        validators.EqualTo('Confirmar', message='as senhas devem ser iguais')
+        validators.EqualTo('confirm', message='As senhas devem ser iguais')
     ])
-    confirmacao = PasswordField('Repita a senha')
+    confirm = PasswordField('Repita a senha')
+
+class LoginForm(Form):
+    email = StringField('Email:', [validators.Length(min=6, max=35)])
+    password = PasswordField('Senha:', [validators.DataRequired()])
+    
