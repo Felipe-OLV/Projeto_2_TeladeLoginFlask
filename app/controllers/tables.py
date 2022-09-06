b030cb6b44de415d259d87c168ffd6206612d557
Felipe <felipemachado.oliveira1@gmail.com>
2022-09-05 22:13:55 -0300

    Tabelas banco de dados

--- app/controllers/tables.py
+++ app/controllers/tables.py
@@ -1,4 +1,5 @@
 from email.policy import default
+from enum import unique
 from sqlalchemy import null
 from app import db
 
@@ -6,11 +7,10 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-    name = db.Column(db.String)
-    email = db.Column(db.String, unique=True)
-    profile = db.Column(db.String, unique=True, default='profile.jpg')
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(180), unique=False, nullable=False)
+    name = db.Column(db.String(40), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __init__(self, username, password, name, email):
         self.username = username
