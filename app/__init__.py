71d5c641783557f6fc8e348c804ef668524baf1d
Felipe <felipemachado.oliveira1@gmail.com>
2022-09-05 22:15:33 -0300

    últimos toques nos controllers da aplicação

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 from config import SQLALCHEMY_DATABASE_URI
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///teste.db'
 app.config['SECRET_KEY'] = 'abcd1234'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,24 +1,38 @@
 from turtle import title
-from flask import render_template, session, request, url_for, flash
+from flask import render_template, session, request, url_for, flash, redirect
 from app import app, db, bcrypt
 from .tables import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
+import os
+
+
+@app.route('/')
+def index():
+    return render_template('teste.html')
+
 
 @app.route('/cadastro', methods=['GET', 'POST'])
 def registrar():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
         hash_password = bcrypt.generate_password_hash(form.password.data)
-        user = User(
-            name=form.name.data, 
-            username=form.username.data, 
-            email=form.email.data,
-            password=form.hash_password.data)
-        db_session.add(user)
-        flash('Registro de conta realizado com sucesso.')
+        user = User(username=form.username.data, name=form.name.data, email=form.email.data,
+        password=hash_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Usuário {form.name.data} cadastrado com sucesso.','sucess')
         return redirect(url_for('login'))
-    return render_template('registrar.html', form=form,title= "Pagina de cadastro")
+    return render_template('registrar.html', form=form,title="Pagina de cadastro")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title="Login")
+    form=LoginForm(request.form)
+    if request.method == "POST" and form.validate():
+        user= User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            session['email'] = form.email.data
+            flash(f'Email: {form.email.data} logado')
+            return redirect(request.args.get('next') or url_for('index'))
+        else:
+            flash(f'ERRO, NÃO FOI POSSIVEL REALIZAR LOGIN')
+    return render_template('login.html',form=form , title="Login")
